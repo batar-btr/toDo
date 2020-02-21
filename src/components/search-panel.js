f9adf3d74f42a4ba2234982d103810e793aee470
unknown <btrn86@gmail.com>
2020-02-21 18:10:32 +0200

    add components

---
+++ src/components/search-panel.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const SearchPanel = () => <input placeholder='search...' type="text" />
+
+export default SearchPanel;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { log } from './funcs/funcs'
+
+import AppHeader from './components/app-header'
+import SearchPanel from './components/search-panel'
+import ToDoList from './components/todo-list'
+
+
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <ToDoList />
+        </div>
+    );
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
